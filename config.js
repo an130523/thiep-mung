31a2313dfdf08f2f294a84d8e9e71f5994f8dc85
sóc <96336623+tranvanana@users.noreply.github.com>
2021-12-18 22:24:41 +0700

    Create config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = 'Gửi em';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'Chúc Mừng noel nhé';
 const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc em luôn tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau nhé ><';
+const contentCard = 'Chúc em luôn tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau nhé!';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
